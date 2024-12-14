db9aefdcec3933f011d186a1a6c70e1e5d517219
Youssef Ayari <youssef.ayari1@esprit.t>
2024-12-14 18:06:30 +0100

    Add jar gpt mail for deploy.

--- src/main/java/de/bsi/openai/ScheduledTasks.java
+++ src/main/java/de/bsi/openai/ScheduledTasks.java
@@ -1,19 +1,18 @@
 package de.bsi.openai;
 
+import de.bsi.openai.chatgpt.PingController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ScheduledTasks {
-    @Autowired
-    private RestTemplate restTemplate;
 
-    //@Scheduled(fixedRate = 5000)
+    @Autowired
+    PingController pingController;
+
+    @Scheduled(fixedRate = 5000)
     public void pingPeriodiquement() {
-        String url = "https://gpt-mail-51yb.onrender.com";
-        String response = restTemplate.getForObject(url, String.class);
-        System.out.println(response);
+        pingController.healthCheck();
     }
 }
