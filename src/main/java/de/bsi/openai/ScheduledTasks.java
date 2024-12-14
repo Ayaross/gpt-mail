ae5d5a1136eba51aa1a5f3a2bec350284dff0046
Youssef Ayari <youssef.ayari1@esprit.t>
2024-12-14 22:03:49 +0100

    Add health check app.

--- src/main/java/de/bsi/openai/ScheduledTasks.java
+++ src/main/java/de/bsi/openai/ScheduledTasks.java
@@ -4,6 +4,7 @@ import de.bsi.openai.chatgpt.PingController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ScheduledTasks {
@@ -11,8 +12,13 @@ public class ScheduledTasks {
     @Autowired
     PingController pingController;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Scheduled(fixedRate = 5000)
     public void pingPeriodiquement() {
-        pingController.healthCheck();
+        String url = "http://localhost:8083/health";
+        String response = restTemplate.getForObject(url, String.class);
+        System.out.println(response);
     }
 }
